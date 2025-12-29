87b82d825f10ce97b984b9f3e8ca1116b80b7135
daniel <daniel.blindr@gmail.com>
2025-12-29 23:38:00 +0000

    memory info panel

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import "./style.css"
 import p5 from "p5"
-import { addStats } from "./library/ui"
+import { addUI } from "./library/ui"
 import sketch from "./sketches/test"
 
-const sketchWithStats = addStats(sketch)
-new p5(sketchWithStats, document.getElementById("app") ?? undefined)
+new p5(addUI(sketch), document.getElementById("app") ?? undefined)
--- library/ui.ts
+++ library/ui.ts
@@ -1,3 +1,4 @@
+import "./ui.css"
 import type p5 from "p5"
 import Stats from "stats-gl"
 import type { Sketch } from "./types"
@@ -22,4 +23,58 @@ export function addStats(sketch: Sketch, node: HTMLElement = document.body): Ske
 			stats.update()
 		}
 	}
+}
+
+interface GMAN_webgl_memory {
+	getMemoryInfo(): { memory: Record<string, number>; resources: Record<string, number> }
+	getResourcesInfo(resource: new () => unknown): { size: number; stackCreated: string }[]
+}
+
+export function addMemoryInfo(sketch: Sketch, node: HTMLElement = document.body): Sketch {
+	let memoryExtension: GMAN_webgl_memory | null = null
+	let memoryInfoPanel: HTMLDivElement
+
+	return (p: p5) => {
+		sketch(p)
+		const { setup, draw } = p
+
+		p.setup = () => {
+			setup()
+			const context = p.drawingContext as WebGL2RenderingContext
+			memoryExtension = context.getExtension("GMAN_webgl_memory") as GMAN_webgl_memory | null
+
+			let isPanelVisible = false
+			memoryInfoPanel = document.createElement("div")
+			memoryInfoPanel.style.display = isPanelVisible ? "block" : "none"
+			memoryInfoPanel.id = "memory-info"
+
+			const memoryInfoToggle = document.createElement("button")
+			memoryInfoToggle.textContent = `${isPanelVisible ? "Hide" : "Show"} Memory Info`
+			memoryInfoToggle.id = "memory-info-toggle"
+
+			const memoryInfoContainer = document.createElement("div")
+			memoryInfoContainer.id = "memory-info-container"
+			memoryInfoContainer.append(memoryInfoPanel, memoryInfoToggle)
+
+			memoryInfoToggle.addEventListener("click", () => {
+				isPanelVisible = !isPanelVisible
+				memoryInfoPanel.style.display = isPanelVisible ? "block" : "none"
+				memoryInfoToggle.textContent = `${isPanelVisible ? "Hide" : "Show"} Memory Info`
+			})
+
+			node.appendChild(memoryInfoContainer)
+		}
+
+		p.draw = () => {
+			draw()
+			if (memoryExtension) {
+				const memoryInfo = memoryExtension.getMemoryInfo()
+				memoryInfoPanel.textContent = JSON.stringify(memoryInfo, null, 2)
+			}
+		}
+	}
+}
+
+export function addUI(sketch: Sketch, node: HTMLElement = document.body): Sketch {
+	return addMemoryInfo(addStats(sketch), node)
 }
