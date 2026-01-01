ffc337a8d13ed17e96b8d406658409e18c4d369e
daniel <daniel.blindr@gmail.com>
2026-01-01 21:19:44 +0000

    One color one shape

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import "./style.css"
 import p5 from "p5"
 import { addUI } from "./library/ui"
-import sketch from "./sketches/test"
+import sketch from "./sketches/2026/1shape1color"
 
 new p5(addUI(sketch), document.getElementById("app") ?? undefined)
