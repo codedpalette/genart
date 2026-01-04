65c89a120687428e49dcf3b48952507646b4f364
daniel <daniel.blindr@gmail.com>
2026-01-04 13:17:24 +0000

    12 principles of animation

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import "./style.css"
 import p5 from "p5"
+import { p5Record } from "p5.record.js"
 import { addUI } from "./library/ui"
-import sketch from "./sketches/2026/1shape1color"
+import sketch from "./sketches/2026/animation"
 
+p5.registerAddon(p5Record)
 new p5(addUI(sketch), document.getElementById("app") ?? undefined)
---
+++ typings/p5.record.js.d.ts
@@ -0,0 +1,16 @@
+import "p5"
+
+declare module "p5.record.js" {
+	// biome-ignore lint/complexity/noBannedTypes: p5Record is only used for passing to p5.registerAddon
+	export declare const p5Record: Function
+}
+
+declare module "p5" {
+	export default interface p5 {
+		startRecording(): void
+		stopRecording(): void
+		pauseRecording(): void
+		resumeRecording(): void
+		createRecording(options: { frameRate: number; source: HTMLCanvasElement; stopCallback: (blob: Blob) => void }): void
+	}
+}
